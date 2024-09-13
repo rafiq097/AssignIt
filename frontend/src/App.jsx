612f448156796699a48529c3e0fa6cc9ac8cc5f3
Rafiq Shaik <rafiqshaik097@gmail.com>
2024-09-13 21:15:25 +0530

    poor auth bro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
 
   return (
     <>
-      
+      <GoogleLogin 
+        onSuccess={(res) => {
+          console.log(res);
+          const token = jwtDecode(res?.credential);
+          console.log(token);
+          //email && given_name
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GoogleOAuthProvider clientId="845080890644-0iidd09rul2fi5k4bbf7q0pv0okm1ppu.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
